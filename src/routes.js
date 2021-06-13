42e7f3cdc232c0108c2d85a30824fcd669c00503
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2021-06-12 21:06:42 -0400

    removed map component

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
-import Maps from "views/examples/Maps.js";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
@@ -21,13 +20,7 @@ var routes = [
     component: Icons,
     layout: "/admin",
   },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "ni ni-pin-3 text-orange",
-    component: Maps,
-    layout: "/admin",
-  },
+
   {
     path: "/user-profile",
     name: "Patient Profile",
